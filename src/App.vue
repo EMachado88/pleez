5d20d894bffd638dc76634dcda54f733b9dbd6c5
Emanuel Machado <emanuel@emachado.pt>
2022-01-29 16:13:29 +0000

    Base layout setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <img alt="Vue logo" src="./assets/logo.png">
+    </main>
+    <Navbar msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  justify-content: space-between;
+}
+
+main {
+  height: calc(100vh - 76px);
+  overflow: auto;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <navbar>
+    <div>
+      Top up
+    </div>
+    <div>
+      Account
+    </div>
+  </navbar>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+navbar {
+  display: flex;
+  justify-content: space-between;
+  height: 76px;
+}
+
+navbar > div {
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
